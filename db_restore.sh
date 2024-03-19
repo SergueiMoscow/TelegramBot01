943d2c9ec5e5d83a27cb48cc4029908ae8d87134
Сергей Сушков <sushkov_sv@ursip.ru>
2024-03-19 10:31:47 +0300

    docker configured

--- db_restore.sh
+++ db_restore.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-if [[ -f "/backup/telegram01.bk" && ! -f "/backup/database.txt" ]]; then
+# это пример подгрузки данных. В докере выключен. Добавлять вручную
+if [[ -f "/app/backup/telegram01.bk" && ! -f "/app/backup/database.txt" ]]; then
     psql -U postgres -d telegram01 < /backup/telegram01.bk
     if [[ $? -eq 0 ]]; then
         echo "Database restore was successful"
-        date > /backup/database.txt
+        date > /app/backup/database.txt
     else
         echo "Database restore failed - check the logs for details"
     fi
